f747d7d4499b6017a335aa3a740b115d5c6c8f1c
Jean Borges <jeanborges946@gmail.com>
2020-06-24 21:38:37 -0300

    Configuraçao basica e instalaçao de plugins

---
+++ config/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: `Projeto Gatsby`,
+  siteUrl: `https://www.projeto-gatsbyjs.com`,
+  description: `Blazing fast modern site generator for React`,
+  social: [
+    {
+      name: 'github',
+      url: 'https://github.com/jeanb762'
+    }
+  ]
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,52 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const siteMetadata = require('./config/metadata');
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Projeto Gatsby`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#7159c1`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`  //512 X 512
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "XYZ-432",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`300`, `500`, `700`]
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`
+  ]
 }
